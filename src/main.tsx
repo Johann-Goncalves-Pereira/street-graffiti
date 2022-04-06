3d1512c36fcddef5bb1f9615a1e02509e0bdd26a
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-04-06 17:29:54 -0300

    feat: Other way of import thing

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App/App";
 
 //& Base Styles, all pages;
-const stylesL01 = import.meta.globEager("./Styles/**/*.scss");
-// import "virtual:fonts.css";
+// const stylesL01 = import.meta.globEager("./Styles/**/*.scss");
+import "./Styles/main.scss";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
