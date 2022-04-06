ce01d7d5d490105ae270f4741b9d2247de861aa9
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-04-05 21:11:16 -0300

    style: :zap: little impruvments

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,17 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx,elm}"],
   theme: {
     colors: {
-      "surface-0": "hsl(0, 0%, 9%)",
-      "surface-1": "hsl(0, 0%, 96%)",
-      "text-0": "hsl(0, 0%, 98%)",
-      "text-0": "hsl(0, 0%, 7%)",
-      "brand-0": "hsl(39, 100%, 50%)",
+      surface: {
+        0: "hsl(0, 0%, 9%)",
+        1: "hsl(0, 0%, 96%)",
+      },
+      text: {
+        0: "hsl(0, 0%, 98%)",
+        1: "hsl(0, 0%, 7%)",
+      },
+      brand: {
+        0: "hsl(39, 100%, 50%)",
+      },
     },
     fontFamily: {
       "fat-wandals": ["var(--font-fat-wandals)"],
